7c2f286ee9ecb00a7832752059dbaaccfba8dcdd
Nicolas Gonzalez <nicolasgonzalez180@gmail.com>
2022-04-22 00:21:12 +0200

    Improve search

--- app/graphql/mutations/search.rb
+++ app/graphql/mutations/search.rb
@@ -4,14 +4,32 @@ module Mutations
 
     argument :q, String, required: true
     argument :page, Integer, required: true
+    argument :folder, String, required: true
+    argument :subfolder, String, required: true
 
     field :results, [Types::FolderType], null: false
     field :current_page, Integer, null: false
     field :total_pages, Integer, null: false
 
     def resolve(args)
-      folders = Folder.order("folder_created_at::timestamp DESC").search(args[:q])
-      paginated_results = Kaminari.paginate_array(folders).page(args[:page]).per(7)
+      if args[:q] && !args[:q].blank?
+        folders = Folder.order("folder_created_at::timestamp DESC")
+                    .where("data_url IS NOT NULL")
+                    .search(args[:q])
+      elsif args[:folder] && !args[:folder].blank? && args[:subfolder] && !args[:subfolder].blank?
+        folders = Folder.order("folder_created_at::timestamp DESC")
+                    .where("data_url IS NOT NULL")
+                    .where(folder: args[:folder], subfolder: args[:subfolder])
+      elsif args[:folder] && !args[:folder].blank?
+        folders = Folder.order("folder_created_at::timestamp DESC")
+                    .where("data_url IS NOT NULL")
+                    .where(folder: args[:folder])
+      else
+        folders = Folder.order("folder_created_at::timestamp DESC")
+                    .where("data_url IS NOT NULL")
+      end
+
+      paginated_results = Kaminari.paginate_array(folders).page(args[:page]).per(15)
 
       MutationResultPaginated.call(
         obj: {
